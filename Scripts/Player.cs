2978f654551e7f60a7afed96c13967ee567058b8
EmmaWasLate <luizgustavohain2007@gmail.com>
2026-02-16 16:26:19 -0300

    menu ta errado
    player existe e vai pra direita

---
+++ Scripts/Player.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	public const float Speed = 300.0f;
+	public const float JumpVelocity = -400.0f;
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+		{
+			velocity += GetGravity() * (float)delta;
+		}
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+		{
+			velocity.Y = JumpVelocity;
+		}
+
+		// Makes the character move right constantly, later this will be changed based on if its inverted or not (1 is right, -1 is left)
+		int direction = 1;
+		velocity.X = direction * Speed;
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
