554fbcc82fe17307de17a56e22a2dba81d68969e
EmmaWasLate <luizgustavohain2007@gmail.com>
2026-02-16 20:45:45 -0300

    Carimbo coloca

--- Entities/Player/Scripts/CarimboManager.cs
+++ Entities/Player/Scripts/CarimboManager.cs
@@ -6,11 +6,14 @@ public partial class CarimboManager : Node
 	[Export]
 	public Carimbo carimboRes;
 
+	[Export] public PackedScene carimbo;
     public override void _Process(double delta)
     {
 		if (Input.IsActionJustPressed("mouse_1"))
 		{
-			
+			Node2D new_carimbo = carimbo.Instantiate<Node2D>();
+			AddChild(new_carimbo);
+			new_carimbo.Position = GetViewport().GetMousePosition();
 		}
     }
 
