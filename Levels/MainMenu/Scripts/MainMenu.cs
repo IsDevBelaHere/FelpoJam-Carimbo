b0ade0cd280f678f278c6b6d2c75641095ebbdc8
IsBelaHere <isabelasszhere@gmail.com>
2026-02-17 03:38:43 -0300

    Carimbo 3D foda no menu

--- Levels/MainMenu/Scripts/MainMenu.cs
+++ Levels/MainMenu/Scripts/MainMenu.cs
@@ -1,9 +1,11 @@
-using Godot;
 using System;
+using Godot;
 
 public partial class MainMenu : MarginContainer
 {
 	[Export] public PackedScene okCarimbo;
+	[Export] public TextureRect carimbo3DTexture;
+	[Export] public AnimationPlayer carimboAnimationPlayer;
 	public Node2D instantiated_okCarimbo;
 	Levels levelSelected;
 	TextureRect rectSelected;
@@ -32,13 +34,22 @@ public partial class MainMenu : MarginContainer
 		rectSelected = null;
 		instantiated_okCarimbo = okCarimbo.Instantiate<Node2D>();
 		AddChild(instantiated_okCarimbo);
-
+		instantiated_okCarimbo.Visible = false;
+		Vector2 placePosition = rect.GlobalPosition + new Vector2(20f,10f);
 		rectScript.SetActive(true);
 		rectSelected = rect;
 
 		levelSelected = (Levels)level;
-
-		instantiated_okCarimbo.GlobalPosition = rect.GlobalPosition + new Vector2(20f,10f);
+		
+		carimbo3DTexture.GlobalPosition = new(placePosition.X - carimbo3DTexture.Size.X/2, placePosition.Y - carimbo3DTexture.Size.Y/2);
+		carimboAnimationPlayer.Play("Stamping");
+		
+	}
+	public void PutAStamp()
+	{
+		Vector2 placePosition = rectSelected.GlobalPosition + new Vector2(20f,10f);
+		instantiated_okCarimbo.GlobalPosition = placePosition;
+		instantiated_okCarimbo.Visible = true;
 	}
 	public void ButtomUp_LevelConfirm()
 	{
